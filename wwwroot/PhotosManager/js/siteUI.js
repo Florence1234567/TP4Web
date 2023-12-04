f9db4913fb547d03550c348f2fa1014d18256ac7
Tristan Berthiaume <tristan.007@live.ca>
2023-12-04 16:53:09 -0500

    Update siteUI.js

--- wwwroot/PhotosManager/js/siteUI.js
+++ wwwroot/PhotosManager/js/siteUI.js
@@ -105,83 +105,129 @@ function renderLogin(){
     });
 }
 
+function createNewUser(){
+    user = {};
+    user.Id = 0;
+    user.Email = "";
+    user.Password = "";
+    user.Name = "";
+    user.Avatar = "";
+    user.VerifyCode = "notVerified";
+    
+    
+    user.Phone = "";
+    
+    return user;
+}
+
 function renderRegister() {
     eraseContent();
     updateHeader("Inscription");
+
+    let newUser = createNewUser();
+    let emailValidation;
+    let passwordValidation;
     $("#content").append(
         $(`
         <form class="form" id="createProfilForm"'>
-        <fieldset>
-        <legend>Adresse ce courriel</legend>
-        <input type="email"
-        class="form-control Email"
-        name="Email"
-        id="Email"
-        placeholder="Courriel"
-        required
-        RequireMessage = 'Veuillez entrer votre courriel'
-        InvalidMessage = 'Courriel invalide'
-        CustomErrorMessage ="Ce courriel est déjà utilisé"/>
-        <input class="form-control MatchedInput"
-        type="text"
-        matchedInputId="Email"
-        name="matchedEmail"
-        id="matchedEmail"
-        placeholder="Vérification"
-        required
-        RequireMessage = 'Veuillez entrez de nouveau votre courriel'
-        InvalidMessage="Les courriels ne correspondent pas" />
-        </fieldset>
-        <fieldset>
-        <legend>Mot de passe</legend>
-        <input type="password"
-        class="form-control"
-        name="Password"
-        id="Password"
-        placeholder="Mot de passe"
-        required
-        RequireMessage = 'Veuillez entrer un mot de passe'
-        InvalidMessage = 'Mot de passe trop court'/>
-        <input class="form-control MatchedInput"
-        type="password"
-        matchedInputId="Password"
-        name="matchedPassword"
-        id="matchedPassword"
-        placeholder="Vérification" required
-        InvalidMessage="Ne correspond pas au mot de passe" />
-        </fieldset>
-        <fieldset>
-        <legend>Nom</legend>
-        <input type="text"
-        class="form-control Alpha"
-        name="Name"
-        id="Name"
-        placeholder="Nom"
-        required
-        RequireMessage = 'Veuillez entrer votre nom'
-        InvalidMessage = 'Nom invalide'/>
-        </fieldset>
-        <fieldset>
-        <legend>Avatar</legend>
-        <div class='imageUploader'
-        newImage='true'
-        controlId='Avatar'
-        imageSrc='images/no-avatar.png'
-        waitingImage="images/Loading_icon.gif">
-        </div>
-        </fieldset>
-        <input type='submit' name='submit' id='saveUserCmd' value="Enregistrer" class="form-control btn-primary">
+            <fieldset>
+            <legend>Adresse ce courriel</legend>
+                <input type="email"
+                class="form-control Email"
+                name="Email"
+                id="Email"
+                placeholder="Courriel"
+                required
+                RequireMessage = 'Veuillez entrer votre courriel'
+                InvalidMessage = 'Courriel invalide'
+                CustomErrorMessage ="Ce courriel est déjà utilisé"
+                value="${newUser.Email}"/>
+
+                <input class="form-control MatchedInput"
+                type="text"
+                matchedInputId="Email"
+                name="matchedEmail"
+                id="matchedEmail"
+                placeholder="Vérification"
+                required
+                RequireMessage = 'Veuillez entrez de nouveau votre courriel'
+                InvalidMessage="Les courriels ne correspondent pas" 
+                value="${emailValidation}"/>
+            </fieldset>
+
+            <fieldset>
+            <legend>Mot de passe</legend>
+                <input type="password"
+                class="form-control"
+                name="Password"
+                id="Password"
+                placeholder="Mot de passe"
+                required
+                RequireMessage = 'Veuillez entrer un mot de passe'
+                InvalidMessage = 'Mot de passe trop court'
+                value="${newUser.Password}"/>
+
+                <input class="form-control MatchedInput"
+                type="password"
+                matchedInputId="Password"
+                name="matchedPassword"
+                id="matchedPassword"
+                placeholder="Vérification" required
+                InvalidMessage="Ne correspond pas au mot de passe" 
+                value="${passwordValidation}"/>
+
+            </fieldset>
+            <fieldset>
+            <legend>Nom</legend>
+                <input type="text"
+                class="form-control Alpha"
+                name="Name"
+                id="Name"
+                placeholder="Nom"
+                required
+                RequireMessage = 'Veuillez entrer votre nom'
+                InvalidMessage = 'Nom invalide'
+                value="${newUser.Name}"/>
+            </fieldset>
+
+            <fieldset>
+            <legend>Avatar</legend>
+                <div class='imageUploader'
+                newImage='true'
+                controlId='Avatar'
+                imageSrc='images/no-avatar.png'
+                waitingImage="${newUser.Avatar}">
+                </div>
+            </fieldset>
+            <input type='submit' name='submit' id='saveUserCmd' value="Enregistrer" class="form-control btn-primary">
         </form>
+
         <div class="cancel">
         <button class="form-control btn-secondary" id="abortCmd">Annuler</button>
         </div>
         
         `)
     )
+    $("#createProfilForm").on("submit", async function(event) {
+        event.preventDefault();
+        let user = getFormData($("#createProfilForm"));
+        showWaitingGif();
+        //API saveUser
+        //Check siteUI ContactsManager
+    })
 
     $("#abortCmd").on("click", function() {
         eraseContent();
         updateHeader("Connexion");
         renderLogin();
     });
+}
+
+function getFormData($form) {
+    const removeTag = new RegExp("(<[a-zA-Z0-9]+>)|(</[a-zA-Z0-9]+>)", "g");
+    var jsonObject = {};
+    $.each($form.serializeArray(), (index, control) => {
+        jsonObject[control.name] = control.value.replace(removeTag, "");
+    });
+    return jsonObject;
 }
