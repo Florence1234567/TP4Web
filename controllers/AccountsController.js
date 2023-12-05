90cd086288f7220cbbbdbcb3c3ae4a4f7465ddb6
Florence1234567 <119359132+Florence1234567@users.noreply.github.com>
2023-12-05 09:19:48 -0500

    .

--- controllers/AccountsController.js
+++ controllers/AccountsController.js
@@ -55,6 +55,8 @@ export default class AccountsController extends Controller {
     }
 
     sendVerificationEmail(user) {
+        // bypass model bindeExtraData wich hide the user verifyCode
+        user = this.repository.findByField("Id", user.Id);
         let html = `
                 Bonjour ${user.Name}, <br /> <br />
                 Voici votre code pour confirmer votre adresse de courriel
@@ -169,7 +171,12 @@ export default class AccountsController extends Controller {
     }
     // GET:account/remove/id
     remove(id) { // warning! this is not an API endpoint
-        if (Authorizations.writeGranted(this.HttpContext, Authorizations.user()))
+        if (Authorizations.writeGranted(this.HttpContext, Authorizations.user())){
+            this.tokensRepository.keepByFilter(token => token.User.Id != id);
+            let previousAuthorization = this.authorizations;
+            this.authorizations = Authorizations.user();
             super.remove(id);
+            this.authorizations = previousAuthorization;
+        }
     }
 }
