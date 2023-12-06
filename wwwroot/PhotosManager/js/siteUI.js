bb9748b08845d5e3631ba72548231023f398efa6
Tristan Berthiaume <tristan.007@live.ca>
2023-12-06 11:46:35 -0500

    fewr

--- wwwroot/PhotosManager/js/siteUI.js
+++ wwwroot/PhotosManager/js/siteUI.js
@@ -85,7 +85,7 @@ function renderAbout() {
 function renderLogin(loginMessage = "") {
     eraseContent();
     updateHeader("Connexion");
-    let EmailError = "";
+    let EmailError = "Courriel introuvable";
     let passwordError = "Mot de passe incorrect";
 
     let user = createNewUser();
@@ -96,12 +96,11 @@ function renderLogin(loginMessage = "") {
             <h3>${loginMessage}</h3>
                 <input type='email'
                        name='Email'
-                       id= 'Email'
                        class='form-control'
                        required
                        RequireMessage = 'Veuillez entrer votre courriel'
                        InvalidMessage = 'Courriel invalide'
-                       placeholder="Adresse de courriel"
+                       placeholder="adresse de courriel"
                        value='${user.Email}'>
                 <span style='color:red'>${EmailError}</span>
                 <input type='password'
@@ -119,19 +118,14 @@ function renderLogin(loginMessage = "") {
                 <button class="form-control btn-info" id="createProfilCmd">Nouveau Compte</button>
             </div>      
         `)
-    );
-
-    document.getElementById("Email").addEventListener("change", (event) => {
-        console.log(API.currentStatus);
-        if(API.currentStatus == 481){
-            EmailError = "Courriel introuvable";
-        }
-      });
+    )
 
     $("#loginForm").on("submit", async function (event) {
         event.preventDefault();
         let user = getFormData($("#loginForm"));
         showWaitingGif();
+        //API saveUser
+        //Check siteUI ContactsManager
 
         let result = await API.login(user.Email, user.password);
         if (result) {
@@ -253,7 +247,7 @@ function renderRegister() {
     $("#createProfilForm").on("submit", async function (event) {
         event.preventDefault();
         let profil = getFormData($("createProfilForm"));
-        showWaitingGif(); 
+        showWaitingGif();
 
         delete profil.matchedEmail;
         delete profil.matchedPassword;
@@ -290,130 +284,4 @@ function renderPhotos() {
         
         `)
     )
-}
-
-function initImageUploaders() {
-    $('.imageUploader').each(function () {
-        let id = $(this).attr('id');
-        let controlId = $(this).attr('controlId');
-        let waitingImage = $(this).attr('waitingImage');
-        let newImage = $(this).attr('newImage') == '1';
-        $(this).css("display","flex");
-        $(this).css("flex-direction","column");
-        $(this).css("align-items","center");
-       // $(this).css("border","1px solid lightgray");
-        $(this).css("border-radius","6px");
-        $(this).css("padding","6px");
-        $(this).css("padding-bottom","3px");
-
-        let imageData = $(this).attr('imageSrc');
-        $(this).append(`<img 
-                         id="${controlId}_UploadedImage" 
-                         name="${controlId}_UploadedImage" 
-                         tabindex=0 
-                         class="UploadedImage"
-                         style="width:100%"
-                         src="${imageData}"
-                         title="Cliquez pour sélectionner un fichier, ou cliquer-déposer une image";
-                         waitingImage ="${waitingImage}">`);
-
-        $(this).append(`<input 
-                         id="${controlId}_ImageUploader" 
-                         type="file" style="visibility:hidden;height:0px;margin:0px !important"
-                         accept="${acceptedFileFormat}">`);
-
-        if (newImage) {
-            $(this).append(`<input 
-                            id="${controlId}" 
-                            name="${controlId}" 
-                            required
-                            RequireMessage ="${missingFileErrorMessage}" 
-                            waitingImage ="${waitingImage}">`);
-        } else {
-            $(this).append(`<input 
-                            id="${controlId}" 
-                            name="${controlId}" 
-                            waitingImage ="${waitingImage}">`);
-        }
-        
-        
-        $(`#${controlId}_UploadedImage`).on('dragenter', function (e) {
-            $(this).css('border', '2px solid blue');
-        });
-
-        $(`#${controlId}_UploadedImage`).on('dragover', function (e) {
-            $(this).css('border', '2px solid blue');
-            e.preventDefault();
-        });
-
-        $(`#${controlId}_UploadedImage`).on('dragleave', function (e) {
-            $(this).css('border', '2px solid white');
-            e.preventDefault();
-        });
-
-        $(`#${controlId}_UploadedImage`).on('drop', function (e) {
-            var image = e.originalEvent.dataTransfer.files[0];
-            $(this).css('background', '#D8F9D3');
-            e.preventDefault();
-            let id = $(this).attr('id').split('_')[0];
-            let UploadedImage = document.querySelector('#' + id + '_UploadedImage');
-            let waitingImage = UploadedImage.getAttribute("waitingImage");
-            let ImageData = document.querySelector('#' + id);
-            // store the previous uploaded image in case the file selection is aborted
-            UploadedImage.setAttribute("previousImage", UploadedImage.src);
-
-            // set the waiting image
-            if (waitingImage !== "") UploadedImage.src = waitingImage;
-            /* take some delai before starting uploading process in order to let browser to update UploadedImage new source affectation */
-            let t2 = setTimeout(function () {
-                if (UploadedImage !== null) {
-                    let len = image.name.length;
-
-                    if (len !== 0) {
-                        let fname = image.name;
-                        //console.log(fname)
-                        let ext = fname.split('.').pop().toLowerCase();
-
-                        if (!validExtension(ext)) {
-                            alert(wrongFileFormatMessage);
-                            UploadedImage.src = UploadedImage.getAttribute("previousImage");
-                        }
-                        else {
-                            let fReader = new FileReader();
-                            fReader.readAsDataURL(image);
-                            fReader.onloadend = () => {
-                                UploadedImage.src = fReader.result;
-                                ImageData.value = UploadedImage.src;
-                                ImageData.setCustomValidity('');
-                            };
-                        }
-                    }
-                    else {
-                        UploadedImage.src = null;
-                    }
-                }
-            }, 30);
-            $(this).css('border', '2px solid white');
-            return true;
-        });
-        ImageUploader_AttachEvent(controlId);
-        let controlIdTop = - $(this).height() / 2;
-        let controlIdLeft = 4;
-        $(`#${controlId}`).css("z-index","-1");
-        $(`#${controlId}`).css("height","0px");
-        $(`#${controlId}`).css("width","0px");
-        $(`#${controlId}`).css("margin","0px");
-        $(`#${controlId}`).css("position","relative");
-        $(`#${controlId}`).css("left",`${controlIdLeft}px`);
-        $(`#${controlId}`).css("top",`${controlIdTop}px`);
-    });
-}
-
-function ImageUploader_AttachEvent(controlId) {
-    // one click will be transmitted to #ImageUploader
-    document.querySelector('#' + controlId + '_UploadedImage').
-        addEventListener('click', () => {
-            document.querySelector('#' + controlId + '_ImageUploader').click();
-        });
-    document.querySelector('#' + controlId + '_ImageUploader').addEventListener('change', preLoadImage);
 }
